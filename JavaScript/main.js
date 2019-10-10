d93a5d7817c26f98fd863fbe8409bbe50bfefb72
MAKAYLA Reese <reese2596@students.cptc.edu>
2019-10-10 14:20:57 -0700

    Added TypeScript File with Student class, Started reg form

---
+++ JavaScript/main.js
@@ -0,0 +1,35 @@
+var Student = (function () {
+    function Student() {
+    }
+    return Student;
+}());
+var stu = new Student();
+stu.firstName = "Makayla";
+var fName = stu.firstName;
+stu.mailingAddress = "123 fake Street, Fake, Fake, 89375";
+window.onload = main;
+function main() {
+    var regBtn = document.querySelector("button");
+    regBtn.onclick = processForm;
+}
+function processForm() {
+    var disciple = getStudentFromForm();
+    displayStudent(disciple);
+    clearForm();
+}
+function getStudentFromForm() {
+    var tempdisciple = new Student();
+    tempdisciple.firstName = getInputValue("first-name");
+    tempdisciple.lastName = getInputValue("last-name");
+    tempdisciple.dateOfBirth = new Date(getInputValue("dob"));
+    tempdisciple.mailingAddress = getInputValue("address");
+    tempdisciple.programOfStudy = getInputValue("program");
+    console.log(tempdisciple);
+    return tempdisciple;
+}
+function getInputValue(id) {
+    var element = document.getElementById(id);
+    return element.value;
+}
+function displayStudent(disciple) { }
+function clearForm() { }
---
+++ TypeScript/main.ts
@@ -0,0 +1,67 @@
+
+/**
+ * Represents a college Student
+ */
+class Student{
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+    /**
+     * street, city, state, zip
+     */
+    mailingAddress: string;
+
+    /**
+     * Program of study
+     */
+    programOfStudy: string;
+
+    //constructor(fName, lName){
+        //this.firstName = fName;
+        //this.lastName = lName;
+    //}
+    
+}
+
+//This is test code
+let stu = new Student();
+stu.firstName = "Makayla";   //set
+let fName = stu.firstName;  //get
+stu.mailingAddress = "123 fake Street, Fake, Fake, 89375"
+//************************************************************ */
+
+window.onload = main;
+
+function main(){
+    let regBtn = document.querySelector("button");
+    regBtn.onclick = processForm;
+}
+
+function processForm(){
+    //Get the student information off the form
+    let disciple:Student = getStudentFromForm();
+    displayStudent(disciple);
+    clearForm();
+}
+
+function getStudentFromForm():Student{
+    let tempdisciple = new Student();
+    tempdisciple.firstName = getInputValue("first-name");
+    tempdisciple.lastName = getInputValue("last-name");
+    tempdisciple.dateOfBirth = new Date(getInputValue("dob"));
+    tempdisciple.mailingAddress = getInputValue("address");
+    tempdisciple.programOfStudy =  getInputValue("program");
+    console.log(tempdisciple);
+    return tempdisciple;
+}
+
+function getInputValue(id:string):string{
+    //return (<HTMLInputElement> document.getElementById(id)).value;
+    let element = <HTMLInputElement> document.getElementById(id);
+    return element.value;
+}
+
+function displayStudent(disciple){}
+
+function clearForm(){}
